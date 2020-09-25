2538a07db4c4848ecc532fbff4b27dbe82fb1132
Sergio Sanchez <sergio.sanchezs@ingenieros.com>
2020-09-25 14:11:30 -0400

    routes on express

--- index.js
+++ index.js
@@ -1,8 +1,52 @@
-// import express = require('express');
-import express from 'express';
+const express = require('express');
+const dotenv = require('dotenv');
+const data = require('./data/data.json');
+dotenv.config(); // loading .env file
 
 const app = express();
-// console.log(process.env.PORT);
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Your server is running on port ${PORT}`));
+// this is for images folder on path /
+app.use(express.static('public'));
+
+// this is for images folder on path images
+app.use('/images', express.static('images'));
+
+app.get('/', (req, res) => {
+  // get data first
+  res.json(data);
+});
+
+app.get(
+  '/item/:id',
+  (req, res, next) => {
+    let userId = Number(req.params.id);
+    console.log(userId);
+    // console.log(data.find(user => user.id == userId));
+    console.log(data[userId]);
+    res.json(data[userId]);
+    next();
+  },
+  (req, res) => console.log('Did you get the right data?'),
+);
+
+app
+  .route('/item')
+  .get(
+    (req, res) => res.send(`A get request with /item route on port ${PORT}`),
+    // res.download('images/rocket.jpg'),
+    // res.redirect('http://www.linkedin.com'),
+    // res.end(),
+  )
+  .post((req, res) =>
+    res.send(`A post request with /item route on port ${PORT}`),
+  )
+  .put((req, res) => res.send(`A put request with /item route on port ${PORT}`))
+  .delete((req, res) =>
+    res.send(`A delete request with /item route on port ${PORT}`),
+  );
+
+app.listen(PORT, () => {
+  console.log(`Your server is running on port ${PORT}`);
+  // console.log(data);
+});
